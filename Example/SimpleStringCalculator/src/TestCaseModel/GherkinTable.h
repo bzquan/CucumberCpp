ce10a7fffc1afc13fc1b3567e13ca4f0cbcb7aef
bzquan <bzquan@gmail.com>
2016-05-08 21:19:40 +0900

    Updated Gherkin table

--- Example/SimpleStringCalculator/src/TestCaseModel/GherkinTable.h
+++ Example/SimpleStringCalculator/src/TestCaseModel/GherkinTable.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <exception>
 
 #include "StringUtility.h"
 
@@ -12,99 +13,70 @@ namespace bdd
 	class GherkinColumn
 	{
 	public:
-		GherkinColumn() : value(L"") {}
-		GherkinColumn(wstring str) : value(str) {}
+		GherkinColumn() : m_Value(L"") {}
+		GherkinColumn(wstring str) : m_Value(str) {}
 
-		int intValue() { return StringUtility::stoi(value); }
-		long longValue() { return StringUtility::stol(value); }
-		double doubleValue() { return StringUtility::stod(value); }
-		wstring wstrValue() { return value; }
-		NString NStrValue() { return StringUtility::wstring2NString(value); }
-		string strValue() { return StringUtility::wstring2string(value); }
-		bool boolValue() { return (value == L"true") || (value == L"TRUE"); }
+		int intValue() { return StringUtility::stoi(m_Value); }
+		long longValue() { return StringUtility::stol(m_Value); }
+		double doubleValue() { return StringUtility::stod(m_Value); }
+		wstring wstrValue() { return m_Value; }
+		NString NStrValue() { return StringUtility::wstring2NString(m_Value); }
+		string strValue() { return StringUtility::wstring2string(m_Value); }
+		bool boolValue() { return (m_Value == L"true") || (m_Value == L"TRUE"); }
 
 		bool operator==(const GherkinColumn& column) const;
 		bool operator!=(const GherkinColumn& column) const;
 
 	private:
-		wstring value;
+		wstring m_Value;
 	};
+
+    class GherkinTable;
 
 	class GherkinRow
 	{
 	public:
-		GherkinRow() {}
-		GherkinRow(wstring row_value) { InitializeColumns(row_value); }
-		int ColumnCount() const { return columns.size(); }
-		GherkinColumn& operator[](int index) { return (index < ColumnCount()) ? columns[index] : nullColumn; }
+		GherkinRow() : m_pTable(nullptr){}
+		GherkinRow(wstring row_value) : m_pTable(nullptr) { InitializeColumns(row_value); }
+        GherkinRow(GherkinTable* pTable, wstring row_value) : m_pTable(pTable) { InitializeColumns(row_value); }
+        int ColumnCount() const { return m_Columns.size(); }
+        GherkinColumn& operator[](int index);
+        GherkinColumn& operator[](wstring col_name);
 
-        std::vector<GherkinColumn>::iterator begin()
-        {
-            return columns.begin();
-        }
-
-        std::vector<GherkinColumn>::iterator end()
-        {
-            return columns.end();
-        }
+        std::vector<GherkinColumn>::iterator begin() { return m_Columns.begin(); }
+        std::vector<GherkinColumn>::iterator end() { return m_Columns.end(); }
 
 		bool operator==(const GherkinRow& row) const;
 		bool operator!=(const GherkinRow& row) const;
 
 	protected:
-		void InitializeColumns(wstring& row_value)
-		{
-			vector<wstring> tokens;
-			StringUtility::Split(tokens, row_value);
-			for (vector<wstring>::iterator it = tokens.begin(); it != tokens.end(); it++)
-			{
-				columns.push_back(GherkinColumn(*it));
-			}
-		}
+        void InitializeColumns(wstring& row_value);
 
 	private:
-		vector<GherkinColumn> columns;
-		static GherkinColumn nullColumn;
+		vector<GherkinColumn> m_Columns;
+        GherkinTable* m_pTable;
 	};
 
 	class GherkinTable
 	{
 	public:
-		int RowCount() const { return rows.size(); }
+        void SetColumnNames(wstring col_names);
+        vector<wstring> ColumnNames() { return m_ColumNames; }
+        int ColIndexFromName(wstring col_name);
 
-		GherkinTable& AddRow(wstring row)
-		{
-			rows.push_back(GherkinRow(row));
-			return *this;
-		}
+		int RowCount() const { return m_Rows.size(); }
 
-		GherkinRow& operator[](int index)
-		{
-			if (index < RowCount())
-			{
-				return rows[index];
-			}
-			else
-			{
-				return nullRow;
-			}
-		}
+        GherkinTable& AddRow(wstring row);
+        GherkinRow& operator[](int index);
 
-		std::vector<GherkinRow>::iterator begin()
-		{
-			return rows.begin();
-		}
-
-		std::vector<GherkinRow>::iterator end()
-		{
-			return rows.end();
-		}
+		std::vector<GherkinRow>::iterator begin() { return m_Rows.begin(); }
+		std::vector<GherkinRow>::iterator end() { return m_Rows.end(); }
 
 		bool operator==(const GherkinTable& table) const;
 
 	private:
-		vector<GherkinRow> rows;
-		static GherkinRow nullRow;
+		vector<GherkinRow> m_Rows;
+        vector<wstring>    m_ColumNames;
 	};
 }
 
